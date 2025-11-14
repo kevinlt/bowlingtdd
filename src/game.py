c5f5d54b5219d009076735b710bdf5cce8e3600a
kevinletexier <kevin.le.texier@gmail.com>
2025-11-14 22:43:08 +0100

    Game score returns 1 when roll 1

--- src/game.py
+++ src/game.py
@@ -1,4 +1,10 @@
+
 class Game:
 
+    _user_score = 0
+
     def score(self) -> int:
-        return 0
+        return self._user_score
+
+    def roll(self, score: int):
+        self._user_score = 1
--- tests/step1.py
+++ tests/step1.py
@@ -5,3 +5,8 @@ class TestStep1:
 
     def test_should_return_0_with_no_roll(self):
         assert Game().score() == 0
+
+    def test_should_return_1_when_roll_1(self):
+        game = Game()
+        game.roll(1)
+        assert game.score() == 1
