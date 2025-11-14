aa0805b851f5388b9a334c9b8a2e8a76b8dd1219
kevinletexier <kevin.le.texier@gmail.com>
2025-11-14 23:44:02 +0100

    Game score returns 20 when rolls 10,1,4,0,0

--- tests/test_step_3.py
+++ tests/test_step_3.py
@@ -5,18 +5,18 @@ class TestStep3:
 
     def test_should_return_11_when_roll_10_0_0_1_0(self):
         game = Game()
-        game.roll(10)
-        game.roll(0)
-        game.roll(0)
-        game.roll(1)
-        game.roll(0)
+        for _ in [10,0,0,1,0]:
+            game.roll(_)
         assert game.score() == 11
 
     def test_should_return_19_when_roll_10_2_0_5_0(self):
         game = Game()
-        game.roll(10)
-        game.roll(2)
-        game.roll(0)
-        game.roll(5)
-        game.roll(0)
+        for _ in [10,2,0,5,0]:
+            game.roll(_)
         assert game.score() == 19
+
+    def test_should_return_20_when_roll_10_1_4_0_0(self):
+        game = Game()
+        for _ in [10,1,4,0,0]:
+            game.roll(_)
+        assert game.score() == 20
